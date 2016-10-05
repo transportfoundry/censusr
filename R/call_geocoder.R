1f378e560213e06a63f17b8012b71c22a55251ef
Josie Kressner <josie@transportfoundry.com>
2016-10-05 15:41:05 -0400

    Add ability to pass in lat/lon as well as addresses

--- R/call_geocoder.R
+++ R/call_geocoder.R
@@ -2,8 +2,9 @@
 #'
 #' Returns GEOID for 2010 geographies.
 #'
-#' @param address A tibble/data frame with the following character columns at
-#'   a minimum: street, city, state.
+#' @param address A tibble/data frame with (at a minimum, others can be present)
+#'   either character columns street, city, and state OR numeric columns lat and
+#'   lon. Lat/lon columns take priority.
 #' @param geoid_type GEOID level to return, \code{c('co', 'tr', 'bg', 'bl')}.
 #'   Defaults to block.
 #'
@@ -19,15 +20,23 @@
 #' @export
 append_geoid <- function(address, geoid = 'bl') {
 
-  # If street, city, or state columns are factors, convert them
-  # Call for each row of the data
-  geoids <- vector(mode="character", length = nrow(address))
-  for (i in 1:nrow(address)) {
-    geoids[i] <- call_geolocator(
-      as.character(address$street[i]),
-      as.character(address$city[i]),
-      as.character(address$state[i])
-      )
+  if ("lat" %in% colnames(address) && "lon" %in% colnames(address)) {
+    # Call for each row of the data
+    geoids <- vector(mode="character", length = nrow(address))
+    for (i in 1:nrow(address)) {
+      geoids[i] <- call_geolocator_latlon(address$lat[i], address$lon[i])
+    }
+  } else {
+    # If street, city, or state columns are factors, convert them
+    # Call for each row of the data
+    geoids <- vector(mode="character", length = nrow(address))
+    for (i in 1:nrow(address)) {
+      geoids[i] <- call_geolocator(
+        as.character(address$street[i]),
+        as.character(address$city[i]),
+        as.character(address$state[i])
+        )
+    }
   }
 
   # Append onto database
@@ -94,5 +103,49 @@ call_geolocator <- function(street, city, state) {
                      ") returned more than one address match. The first match was returned."))
     }
     return(response$result$addressMatches[[1]]$geographies$`Census Blocks`[[1]]$GEOID)
+  }
+}
+
+
+#' Call gelocator for one address with lat/lon
+#'
+#' @param lat A numeric value
+#' @param lon A numeric value
+#'
+#' @return A character string representing the Census block of the supplied
+#'   lat/lon.
+#'
+#' importFrom utils URLencode
+#' importFrom httr GET stop_for_status
+#'
+#' @export
+#'
+call_geolocator_latlon <- function(lat, lon) {
+  # Build url
+  call_start <- "https://geocoding.geo.census.gov/geocoder/geographies/coordinates?"
+
+  url <- paste0(
+    "x=", lon,
+    "&y=", lat
+  )
+
+  call_end <- "&benchmark=Public_AR_Census2010&vintage=Census2010_Census2010&layers=14&format=json"
+
+  url_full <- paste0(call_start, url, call_end)
+
+  # Check response
+  r <- httr::GET(url_full)
+  httr::stop_for_status(r)
+  response <- httr::content(r)
+  if (length(response$result$geographies$`Census Blocks`) == 0) {
+    message(paste0("Lat/lon (", lat, ", ", lon,
+                   ") returned no geocodes. An NA was returned."))
+    return(NA_character_)
+  } else {
+    if (length(response$result$geographies$`Census Blocks`) > 1) {
+      message(paste0("Lat/lon (", lat, ", ", lon,
+                     ") returned more than geocode. The first match was returned."))
+    }
+    return(response$result$geographies$`Census Blocks`[[1]]$GEOID)
   }
 }
